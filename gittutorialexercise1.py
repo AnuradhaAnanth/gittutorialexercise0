392f328403e7aee4a35d6fd288c3c31e553e59a9
[Anuradha] <[anuradhaananth536@gmail.com]>
2020-08-21 12:36:24 +0530

    This is the second exercise

---
+++ gittutorialexercise1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+def print_message():
+    print("Hi this is the first output")
+    print_message()
+
+
+# In[6]:
+
+
+print_message()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
